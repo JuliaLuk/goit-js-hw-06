aef5fc4210afd4a608f88741aa9a06ce395ce61d
JuliaLuk <julia1lukasiewicz@gmail.com>
2023-10-31 13:06:22 -0500

    dz 1

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,14 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const itemEl = document.createElement("li");
+itemEl.classList.add("item");
+itemEl.textContent = ingredients[0];
+const ingrList = document.querySelector("#ingredients");
+ingrList.appenndChild(itemEl);
