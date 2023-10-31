fb6a11c6389ae393c8f3266998632357aff002b0
JuliaLuk <julia1lukasiewicz@gmail.com>
2023-10-31 11:17:02 -0500

    add wychidni fajly

---
+++ code.js
@@ -0,0 +1,16 @@
+// Zawdannia 1
+
+// Zawdannia 2
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+const itemEl = document.createElement("li");
+itemEl.classList.add("item");
+itemEl.textContent = ingredients[0];
+const ingrList = document.querySelector("#ingredients");
+// ingrList.appenndChild(itemEl);
