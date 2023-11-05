5ff66d17adad31c50aa99041bd17edb958d60e09
JuliaLuk <julia1lukasiewicz@gmail.com>
2023-11-05 17:53:36 -0600

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeBtn = document.querySelector(".change-color");
+// console.log(changeBtn);
+
+changeBtn.addEventListener("click", handelChange);
+
+function handelChange(event) {
+  if (event) {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }
+}
