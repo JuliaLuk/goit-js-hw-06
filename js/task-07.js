28a974ead210b1d13616f9d6503a189680a982ca
JuliaLuk <julia1lukasiewicz@gmail.com>
2023-11-09 12:33:49 -0600

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -3,7 +3,7 @@ console.log(slider);
 
 const sliderText = document.querySelector("#text");
 // console.log(sliderText);
-
+sliderText.style.fontSize = `${slider.value}px`;
 slider.addEventListener("input", () => {
   sliderText.style.fontSize = `${slider.value}px`;
 });
